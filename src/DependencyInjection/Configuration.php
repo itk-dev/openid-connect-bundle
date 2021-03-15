d49a19613691ea635cd95ddfac5abc5184b17c98
Jeppe Kuhlmann Andersen <jeppeka@Jeppes-MacBook-Pro.local>
2021-03-15 12:45:07 +0100

    OpenId-Connect-Bundle: Added dependency configuration file and getPath to bundle file

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ItkDev\OpenIdConnectBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('itk_dev_openid_connect');
+
+        return $treeBuilder;
+    }
+}
