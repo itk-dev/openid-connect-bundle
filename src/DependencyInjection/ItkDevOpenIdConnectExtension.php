0d2c4dd4342cdbeea631c0b1380dc76060c0e740
Jeppe Kuhlmann Andersen <jeppeka@Jeppes-MacBook-Pro.local>
2021-03-15 12:55:16 +0100

    OpenId-Connect-Bundle: Added initial bundle extension

--- src/DependencyInjection/ItkDevOpenIdConnectExtension.php
+++ src/DependencyInjection/ItkDevOpenIdConnectExtension.php
@@ -1,1 +1,14 @@
 <?php
+
+namespace ItkDev\OpenIdConnectBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class ItkDevOpenIdConnectExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // ... you'll load the files here later
+    }
+}
