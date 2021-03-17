229e3c5523d982769c72fb107f8695520f5d955d
Jeppe Kuhlmann Andersen <jeppeka@Jeppes-MacBook-Pro.local>
2021-03-17 10:28:14 +0100

    OpenId-Connect-Bundle: Initial configuration tree and config/bundles file

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    // ...
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    ItkDev\OpenIdConnectBundle\ItkDevOpenIdConnectBundle::class => ['all' => true],
+];
--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,6 +17,30 @@ class Configuration implements ConfigurationInterface
         // Specify which variables must be configured in itk_dev_openid_connect file
         // That is client_id, client_secret, discovery url and cache path
 
+        $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('openIdProviderOptions')
+                    ->children()
+                        ->scalarNode('open_id_provider_url')
+                            ->info('URL to OpenId Discovery Document')
+                            ->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('open_id_provider_client_id')
+                            ->info('Client ID assigned by authorizer')
+                            ->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('open_id_provider_client_secret')
+                            ->info('Client secret/password assigned by authorizer')
+                            ->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('open_id_provider_cache_path')
+                            ->info('Path for caching Discovery document')
+                            ->defaultValue('%kernel.cache_dir%/.well_known_cache.php')
+                            ->isRequired()->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+                ->scalarNode('default_uri')
+                    ->defaultValue('https://127.0.0.1:8000')
+                    ->isRequired()->cannotBeEmpty()->end()
+            ->end();
+
         return $treeBuilder;
     }
 }
